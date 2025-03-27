7fdc5b96949af02667188ffd23f373813967c2a6
minjunpark <mj9457@naver.com>
2025-03-27 17:24:40 +0900

    Chore: ESLint, Prettier, Tailwind, PostCSS 설정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,41 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import js from '@eslint/js';
+import nextPlugin from '@next/eslint-plugin-next';
+import airbnbConfig from 'eslint-config-airbnb';
+import airbnbHooksConfig from 'eslint-config-airbnb/hooks';
+import prettierConfig from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default [
+  js.configs.recommended,
+  ...airbnbConfig.extends,
+  ...airbnbHooksConfig.extends,
+  {
+    plugins: {
+      '@next/next': nextPlugin,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.jsx'] }],
+      'import/extensions': [
+        'error',
+        'ignorePackages',
+        {
+          js: 'never',
+          jsx: 'never',
+          ts: 'never',
+          tsx: 'never',
+        },
+      ],
+      'prettier/prettier': 'error',
+    },
+    settings: {
+      'import/resolver': {
+        node: {
+          extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        },
+      },
+    },
+  },
+  prettierConfig,
 ];
-
-export default eslintConfig;
