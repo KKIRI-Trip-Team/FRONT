2d8eeaf89c2977281027c06889d03586c5b1569b
minjunpark <mj9457@naver.com>
2025-04-26 15:27:16 +0900

    Style : 기본 font 추가, breakpoints 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,22 @@ export const content = [
   './src/components/**/*.{js,ts,jsx,tsx}',
 ];
 export const theme = {
-  extend: {},
+  screens: {
+    mb: '375px',
+    // => @media (min-width: 375px) { ... }
+
+    tb: '768px',
+    // => @media (min-width: 768px) { ... }
+
+    pc: '1440px',
+    // => @media (min-width: 1440px) { ... }
+  },
+  extend: {
+    fontFamily: {
+      pretendard: ['Pretendard', 'sans-serif'],
+      // 기본 폰트로 설정하고 싶다면 아래 줄 추가
+      sans: ['Pretendard', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+    },
+  },
 };
 export const plugins = [];
